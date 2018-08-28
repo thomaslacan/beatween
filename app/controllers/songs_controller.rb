131e19bb9a541a766b6e8ea475d6a57216a9459b
antoine duvauchelle <antoine@goodmorningcoconut.co>
2018-08-28 11:46:19 +0200

    songs show basics done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,16 @@
 class SongsController < ApplicationController
+  before_action :skip_authorization
+  before_action :set_song, only: [ :show ]
+
   def index
     @songs = policy_scope(Song)#.order(id: integer, name: string, description: text, bpm: integer, num_of_tracks: integer, duration: integer, user_id: integer, genre_id: integer, created_at: datetime, updated_at: datetime, photo: string)
   end
 
   def show
+    duration = (@song.duration.to_f / 60000).to_s
+    split = duration.split(".")
+    @minutes = split[0]
+    @seconds = split[1]
   end
 
   def new
@@ -16,5 +23,11 @@ class SongsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
   end
 end
