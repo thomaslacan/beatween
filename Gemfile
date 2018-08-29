5414f8c1fe08254780a86b4291702e282635a2f9
antoine duvauchelle <antoine@goodmorningcoconut.co>
2018-08-29 18:04:12 +0200

    pg search basics

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'pundit'
 
 gem 'cloudinary'
 gem 'carrierwave', '~> 1.2'
+gem 'pg_search'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_112335) do
+ActiveRecord::Schema.define(version: 2018_08_29_143638) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_112335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "track_url"
+    t.string "instrument_type"
     t.index ["user_id"], name: "index_tracks_on_user_id"
   end
 
