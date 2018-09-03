247d96faf9051496d75450759e0296f1066ef7e7
romain sanson <romainsanson.gm@gmail.com>
2018-09-03 14:34:29 +0200

    pending tracks

--- app/controllers/song_tracks_controller.rb
+++ app/controllers/song_tracks_controller.rb
@@ -9,6 +9,14 @@ class SongTracksController < ApplicationController
 
   end
 
+  def update
+    @song_track = SongTrack.find(params[:id])
+    @song = @song_track.song
+    @song_track.status = 'active'
+    @song_track.save
+    redirect_to song_path(@song)
+  end
+
   def destroy
     @song_track = SongTrack.find(params[:id])
     @song = @song_track.song
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'songs#index', as: "discover"
   resources :songs, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :song_tracks, only: [:new, :create, :edit, :update]
+    resources :song_tracks, only: [:new, :create]
     resources :tracks, only: [ :create, :new ]
   end
   resources :users, only: [:show, :edit, :update]
   resources :tracks, only: [ :destroy, :edit, :update]
-  resources :song_tracks, only: [ :destroy ]
+  resources :song_tracks, only: [ :destroy,  :edit, :update ]
 end
