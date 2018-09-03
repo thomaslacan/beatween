45c5ca1409bdd3ab3618a4985dca4b32ee104e68
antoine duvauchelle <antoine@goodmorningcoconut.co>
2018-09-03 13:41:27 +0200

    fixed footer and navbar

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,14 +1,14 @@
 const searchButton = document.getElementById('search-button');
 const cardBoard = document.querySelector('.cards-board');
 const searchBar = document.getElementById('search-bar');
-const burgerMenu = document.getElementById('menu-menu');
+const navBar = document.getElementById('navbar-navbar');
 
 function appendSearch() {
   searchButton.addEventListener('click', (event) => {
     cardBoard.classList.toggle('cards-minimized');
     searchBar.classList.toggle('invisible');
     searchButton.classList.toggle('distorted');
-    burgerMenu.classList.toggle('distorted');
+    navBar.classList.toggle('navbar-minimized');
   });
 }
 
