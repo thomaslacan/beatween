1cc72ccf0e7813aaa9d7b58c9aa32efa99500bd1
antoine duvauchelle <antoine@goodmorningcoconut.co>
2018-09-04 11:33:48 +0200

    fixed search once and for all

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
+import $ from "jquery";
+window.$ = $;
+window.jQuery = $;
 import {toggleContent} from './card.js';
 import {appendSearch} from './search.js';
 import "./player/browser.js";
