61d5221f61574906652824008a347a4d3f287911
antoine duvauchelle <antoine@goodmorningcoconut.co>
2018-09-07 11:59:43 +0200

    deleted console logs

--- app/javascript/packs/tooltip.js
+++ app/javascript/packs/tooltip.js
@@ -2,13 +2,11 @@ const userPictures = document.querySelectorAll('.track-user-picture');
 
 userPictures.forEach((picture) => {
   picture.addEventListener('mouseover', (event) => {
-    console.log('mouseover')
     let tooltip = event.target.parentElement.parentElement.childNodes[1];
     tooltip.classList.remove('hidden');
   });
 
   picture.addEventListener('mouseout', (event) => {
-    console.log('mouseout')
     let tiptool = event.target.parentElement.parentElement.childNodes[1];
     tiptool.classList.add('hidden');
   });
